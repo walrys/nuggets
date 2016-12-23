69e4ce4b92b0bad255933c853876de14a6b59005
bsweeyee <sweeyee3@gmail.com>
2016-12-23 11:09:59 +0800

    added JUnit testing into android studios

---
+++ core/src/test/java/com/bnw/nuggetdance/NuggetsTest.java
@@ -0,0 +1,26 @@
+package com.bnw.nuggetdance;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.fail;
+
+/**
+ * Created by Brandon on 22/12/16.
+ */
+public class NuggetsTest {
+
+    @Test
+    public void testCreate() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void testRender() throws Exception {
+
+    }
+
+    @Test
+    public void testDispose() throws Exception {
+
+    }
+}
